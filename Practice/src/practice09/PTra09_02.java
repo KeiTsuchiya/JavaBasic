3a342e1c086a70645ad3eb7985fd47ec323d3df0
土屋圭 <k.tsuchiya418@gmail.com>
2018-08-14 15:07:20 +0900

    9-2

--- Practice/src/practice09/PTra09_02.java
+++ Practice/src/practice09/PTra09_02.java
@@ -9,12 +9,13 @@ package practice09;
 public class PTra09_02 {
 	public static void main(String[] args) {
 		String[] array = {"Plain", "Island", "Mountain", "Swamp", "Forest"};
-
+		
 		// ★ 配列arrayを、Util内のメソッドを用いてランダムに並び替えてください
 		// ※ Utilはインポートしても良いし、完全限定名で記述しても良いです
-
-
+		String[] mix = practice09.util.Util.shuffleArray(array);
 		// ★ ランダムに並び替えた後の配列を前から順に全て表示してください
-
+		for(int i = 0;i < mix.length;i++) {
+			System.out.print(mix[i] + " ");
+		}
 	}
 }
