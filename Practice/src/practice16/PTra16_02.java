0377f8cca85b84779783096841f4b90386f2c746
土屋圭 <k.tsuchiya418@gmail.com>
2018-09-07 12:09:58 +0900

    16-2

--- Practice/src/practice16/PTra16_02.java
+++ Practice/src/practice16/PTra16_02.java
@@ -10,13 +10,28 @@ public class PTra16_02 {
 	public static void main(String[] args) {
 
 		boolean b = true;
+		Boolean b2 = Boolean.valueOf(b);
+
 		byte by = 100;
+		Byte by2 = by;
+
 		short s = 300;
+		Short s2 = Short.valueOf(s);
+
 		char c = '日';
+		Character c2 = c;
+
 		int i = 1000;
+		Integer i2 = Integer.valueOf(i);
+
 		long l = 2000L;
+		Long l2 = l;
+
 		float f = 3.14F;
+		Float f2 = Float.valueOf(f);
+
 		double d = 453.592;
+		Double d2 = d;
 
 		// ★ 上記基本型を、それぞれのラッパークラスに代入してください
 	}
