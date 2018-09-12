e7eaed4e8987ce0e8c9dc392bd8797c519fb5cd1
土屋圭 <k.tsuchiya418@gmail.com>
2018-09-12 11:59:22 +0900

    17-4

--- Practice/src/practice17/ptra17/PTra17_04.java
+++ Practice/src/practice17/ptra17/PTra17_04.java
@@ -10,11 +10,19 @@ public class PTra17_04 {
 	public static void main(String[] args) {
 
 		String str = "スッキリわかるJava入門";
-
 		/*
 		 * ★ strに入っている文字列を1文字ずつゆっくり表示させるプログラムを完成させてください
 		 * ※ Thread.sleep を使用すること
 		 */
-
+		char data[] = str.toCharArray();
+		int arr_num = data.length;
+		for(int i = 0;i <= arr_num-1; i++) {
+			try {
+				Thread.sleep(150);
+			}catch(InterruptedException e) {
+				System.out.println("例外が発生しました");
+			}
+			System.out.print(data[i]);
+		}
 	}
 }
